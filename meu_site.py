3f8267976d08c4c0f7a498d1be1b85ed503237d3
Sergio-Henrique49 <126737592+Sergio-Henrique49@users.noreply.github.com>
2023-04-09 21:00:40 -0300

    tentativa-01
    tentativa de upar o site desafio com o flask, porém os links estão quebrados

---
+++ meu_site.py
@@ -0,0 +1,19 @@
+from  flask import Flask, render_template
+
+app = Flask (__name__)
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/contatos")
+def contatos():
+    return render_template("contato.html")
+
+@app.route("quem_somos")
+def quem_somos():
+    return render_template("quem.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+app.run()
