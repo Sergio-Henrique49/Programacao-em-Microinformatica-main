6c35b970d0af7402ff5351e43e658600546fb8a8
Sergio-Henrique49 <126737592+Sergio-Henrique49@users.noreply.github.com>
2023-04-09 21:44:25 -0300

    funfou

--- meuprojeto/venv/app.py
+++ meuprojeto/venv/app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home.html")
 def homepage():
     return render_template("home.html")
 
-@app.route("/contatos")
+@app.route("/contatos.html")
 def contatos():
     return render_template("contatos.html")
 
-@app.route("/quem_somos")
+@app.route("/quem_somos.html")
 def quem_somos():
     return render_template("quem_somos.html")
 
